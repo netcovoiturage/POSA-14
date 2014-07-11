7dd57243df9ace2018eee44afb5ffcdfa9343017
netcovoiturage <netcovoiturage@gmail.com>
2014-07-12 02:57:07 +0400

    w7

--- assignments/week-7-assignment-6/iRemember/src/edu/vuum/mocca/ui/LoginActivity.java
+++ assignments/week-7-assignment-6/iRemember/src/edu/vuum/mocca/ui/LoginActivity.java
@@ -25,9 +25,6 @@ public class LoginActivity extends StoryActivityBase{
 	EditText mLoginId;
 	EditText mPassword;
 	
-	// Make sure we use maximum security to store login credentials
-	static final int MAX_SECURITY = Integer.MAX_VALUE;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,9 +42,10 @@ public class LoginActivity extends StoryActivityBase{
 	 * Get the file used for storing login credentials
 	 */
 	public static File getLoginFile (Context context) {
+		//Use SECURITY_PRIVATE from StorageUtilities
 		return StorageUtilities.getOutputMediaFile(context, 	// Line 48
 				StorageUtilities.MEDIA_TYPE_TEXT, 
-				MAX_SECURITY, 
+				StorageUtilities.SECURITY_PRIVATE, 
 				"login.txt");
 	}
 	
